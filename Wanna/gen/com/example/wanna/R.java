b3c98a48f2c349c3f4904e118d4fc2aea91e1a31
AA-COMP231-S63G1 <abrahamian10@gmail.com>
2014-10-22 20:47:13 -0400

    Create event form and upload image function

--- Wanna/gen/com/example/wanna/R.java
+++ Wanna/gen/com/example/wanna/R.java
@@ -8,6 +8,9 @@
 package com.example.wanna;
 
 public final class R {
+    public static final class array {
+        public static final int createEventArray=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,50 +28,70 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int CustomFontText=0x7f080000;
-        public static final int Retype_Password=0x7f08000f;
-        public static final int action_settings=0x7f08001a;
-        public static final int age=0x7f080015;
-        public static final int buttonCancle=0x7f080019;
-        public static final int buttonDone=0x7f080018;
-        public static final int buttonEnter=0x7f08000a;
-        public static final int description=0x7f080017;
-        public static final int editText1=0x7f08000e;
-        public static final int editText2=0x7f080010;
-        public static final int genderFemale=0x7f080013;
-        public static final int genderMale=0x7f080012;
-        public static final int loginButton=0x7f080008;
-        public static final int loginNotification=0x7f080009;
-        public static final int loginPasswordNotification=0x7f080007;
-        public static final int loginUserNameNotification=0x7f080004;
-        public static final int name=0x7f08000d;
-        public static final int password=0x7f080006;
-        public static final int register=0x7f080001;
-        public static final int tvAge=0x7f080014;
-        public static final int tvDescription=0x7f080016;
-        public static final int tvGender=0x7f080011;
-        public static final int tvName=0x7f08000c;
-        public static final int tvPassword=0x7f080005;
-        public static final int tvUsername=0x7f080002;
-        public static final int userPitcure=0x7f08000b;
-        public static final int username=0x7f080003;
+        public static final int CustomFontText=0x7f090013;
+        public static final int Retype_Password=0x7f090021;
+        public static final int action_settings=0x7f09002b;
+        public static final int age=0x7f090026;
+        public static final int btnSelectImage=0x7f090004;
+        public static final int buttonCancle=0x7f09002a;
+        public static final int buttonCreateEvent=0x7f090012;
+        public static final int buttonDone=0x7f090029;
+        public static final int buttonEnter=0x7f09001d;
+        public static final int description=0x7f090028;
+        public static final int editText1=0x7f090011;
+        public static final int editText2=0x7f090009;
+        public static final int editText3=0x7f09000d;
+        public static final int eventDate=0x7f090007;
+        public static final int eventName=0x7f090003;
+        public static final int eventPrice=0x7f09000f;
+        public static final int eventVenue=0x7f09000b;
+        public static final int genderFemale=0x7f090024;
+        public static final int genderMale=0x7f090023;
+        public static final int imgView=0x7f090005;
+        public static final int loginButton=0x7f09001b;
+        public static final int loginNotification=0x7f09001c;
+        public static final int loginPasswordNotification=0x7f09001a;
+        public static final int loginUserNameNotification=0x7f090017;
+        public static final int name=0x7f090020;
+        public static final int password=0x7f090019;
+        public static final int register=0x7f090014;
+        public static final int scrollView1=0x7f090000;
+        public static final int spinnerEventType=0x7f090001;
+        public static final int tvAge=0x7f090025;
+        public static final int tvDescription=0x7f090027;
+        public static final int tvEventDate=0x7f090006;
+        public static final int tvEventDescription=0x7f090010;
+        public static final int tvEventLocation=0x7f09000c;
+        public static final int tvEventName=0x7f090002;
+        public static final int tvEventPrice=0x7f09000e;
+        public static final int tvEventTime=0x7f090008;
+        public static final int tvEventVenue=0x7f09000a;
+        public static final int tvGender=0x7f090022;
+        public static final int tvName=0x7f09001f;
+        public static final int tvPassword=0x7f090018;
+        public static final int tvUsername=0x7f090015;
+        public static final int userPitcure=0x7f09001e;
+        public static final int username=0x7f090016;
     }
     public static final class layout {
-        public static final int activity_home_page=0x7f030000;
-        public static final int activity_login=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_profile_form=0x7f030003;
+        public static final int activity_create_event=0x7f030000;
+        public static final int activity_home_page=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_profile_form=0x7f030004;
     }
     public static final class menu {
-        public static final int home_page=0x7f070000;
-        public static final int login=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int create_event=0x7f080000;
+        public static final int home_page=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int main=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int activity_profile_form=0x7f05001e;
         public static final int age=0x7f05001a;
         public static final int app_name=0x7f050000;
+        public static final int buttonCreateEvent=0x7f050023;
         public static final int cancel=0x7f05001d;
         public static final int description=0x7f05001b;
         public static final int done=0x7f05001c;
@@ -85,6 +108,9 @@ public final class R {
         public static final int genderFemale=0x7f050019;
         public static final int genderMale=0x7f050018;
         public static final int hello_world=0x7f050001;
+        public static final int hintEventDate=0x7f050021;
+        public static final int hintEventTime=0x7f050020;
+        public static final int hintPrice=0x7f050022;
         public static final int login=0x7f050007;
         public static final int loginButton=0x7f05000d;
         public static final int name=0x7f05000a;
@@ -94,6 +120,7 @@ public final class R {
         public static final int title_activity_create_event=0x7f05000e;
         public static final int title_activity_home_page=0x7f050006;
         public static final int title_activity_register_login=0x7f050005;
+        public static final int uploadImage=0x7f05001f;
         public static final int username=0x7f050009;
         public static final int welcome=0x7f050003;
     }
@@ -119,10 +146,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int FormTheme=0x7f070002;
+        public static final int SpinnerTextAppearance=0x7f070003;
     }
 }
