427c5066e00212b5ab8336ab549510c6fc851670
AA-COMP231-S63G1 <abrahamian10@gmail.com>
2014-11-02 21:16:10 -0500

    Edit event information form

--- Wanna/src/com/example/wanna/library/UserFunctions.java
+++ Wanna/src/com/example/wanna/library/UserFunctions.java
@@ -2,9 +2,11 @@ package com.example.wanna.library;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
+
 import android.content.Context;
 
 
