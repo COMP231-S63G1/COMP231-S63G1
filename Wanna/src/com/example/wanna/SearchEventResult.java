5adc5d37cfd27e0b51703c537f78cd174a733c9d
Darren Liu <gulang15@gmail.com>
2014-11-15 01:56:12 -0500

    Search group java

--- Wanna/src/com/example/wanna/SearchEventResult.java
+++ Wanna/src/com/example/wanna/SearchEventResult.java
@@ -31,11 +31,12 @@ public class SearchEventResult extends ListActivity {
 	JSONParser jsonParser = new JSONParser();
 	UserFunctions userFunctions = new UserFunctions();
 	private ProgressDialog pDialog;
-	
+
 	// url to view event detail
-	private String urlSearchEvent = userFunctions.URL_ROOT + "DB_SearchEvent.php";
+	private String urlSearchEvent = userFunctions.URL_ROOT
+			+ "DB_SearchEvent.php";
 	ListView lvEventItem;
-	
+
 	ArrayList<String[]> eventItemsList = new ArrayList<String[]>();
 	ListViewAdapter searchEventAdapter;
 	String eventID;
@@ -43,10 +44,10 @@ public class SearchEventResult extends ListActivity {
 	int success;
 	String message;
 	String searchEventName;
-	
-	// products JSONArray
+
+	// event JSONArray
 	JSONArray eventList = null;
-	
+
 	// JSON Node names
 	private static final String TAG_SUCCESS = "success";
 	private static final String TAG_MESSAGE = "message";
@@ -54,7 +55,7 @@ public class SearchEventResult extends ListActivity {
 	private static final String TAG_EVENTID = "eventID";
 	private static final String TAG_EVENTNAME = "eventName";
 	private static final String TAG_SEARCHEVENTNAME = "searchEventName";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -62,18 +63,19 @@ public class SearchEventResult extends ListActivity {
 		Intent intent = getIntent();
 		searchEventName = intent.getStringExtra(TAG_SEARCHEVENTNAME);
 		new SearchEventTask().execute();
-		lvEventItem = (ListView)findViewById(android.R.id.list);
+		lvEventItem = (ListView) findViewById(android.R.id.list);
 		searchEventAdapter = new ListViewAdapter(eventItemsList, this);
 		lvEventItem.setAdapter(searchEventAdapter);
 	}
-	
+
 	public void onListItemClick(ListView l, View v, int position, long id) {
-	    String eventID = (String) searchEventAdapter.getItem(position);
-		Intent intent = new Intent(getApplicationContext(), ViewAndJoinEvent.class);
+		String eventID = (String) searchEventAdapter.getItem(position);
+		Intent intent = new Intent(getApplicationContext(),
+				ViewAndJoinEvent.class);
 		intent.putExtra("eventID", eventID);
-		startActivity(intent);	
-	  }
-	
+		startActivity(intent);
+	}
+
 	private class SearchEventTask extends AsyncTask<String, Void, String> {
 		@Override
 		protected void onPreExecute() {
@@ -85,50 +87,55 @@ public class SearchEventResult extends ListActivity {
 			pDialog.setCancelable(true);
 			pDialog.show();
 		}
-		
+
 		@Override
 		protected String doInBackground(String... urls) {
 			// Building Parameters
 			List<NameValuePair> searchEventParams = new ArrayList<NameValuePair>();
-			searchEventParams.add(new BasicNameValuePair("searchEventName", searchEventName));
+			searchEventParams.add(new BasicNameValuePair("searchEventName",
+					searchEventName));
 			// getting event details by making HTTP request
-			JSONObject json = jsonParser.getJSONFromUrl(
-					urlSearchEvent, searchEventParams);
+			JSONObject json = jsonParser.getJSONFromUrl(urlSearchEvent,
+					searchEventParams);
 			// json success tag
 			success = json.optInt(TAG_SUCCESS);
 			message = json.optString(TAG_MESSAGE);
-			if (success == 1) {	
+			if (success == 1) {
 				eventList = json.optJSONArray(TAG_EVENTLIST);
 				// looping through All Products
 				for (int i = 0; i < eventList.length(); i++) {
 					JSONObject event = eventList.optJSONObject(i);
 					eventID = event.optString(TAG_EVENTID);
 					eventName = event.optString(TAG_EVENTNAME);
-					String[] eventItems = {eventID, eventName};
+					String[] eventItems = { eventID, eventName };
 					eventItemsList.add(eventItems);
-//				// successfully received event details
-//				JSONArray searchEventArray = json.optJSONArray(TAG_EVENTLIST);
-//				List<String> eventArrayList = new ArrayList<String>();
-//				for(int i = 0; i < searchEventArray.length(); i++){
-//					try {
-//						JSONObject jsonObject = searchEventArray.getJSONObject(i);
-//						eventArrayList.add(jsonObject.toString());
-//					} catch (JSONException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-//					testEventItems = eventArrayList.toArray(new String[eventArrayList.size()]);
+					// // successfully received event details
+					// JSONArray searchEventArray =
+					// json.optJSONArray(TAG_EVENTLIST);
+					// List<String> eventArrayList = new ArrayList<String>();
+					// for(int i = 0; i < searchEventArray.length(); i++){
+					// try {
+					// JSONObject jsonObject =
+					// searchEventArray.getJSONObject(i);
+					// eventArrayList.add(jsonObject.toString());
+					// } catch (JSONException e) {
+					// // TODO Auto-generated catch block
+					// e.printStackTrace();
+					// }
+					// testEventItems = eventArrayList.toArray(new
+					// String[eventArrayList.size()]);
 				}
-			}else{
+			} else {
 			}
 			return null;
-			}
-		
+		}
+
 		@Override
 		protected void onPostExecute(String result) {
-			Toast.makeText(getApplicationContext(),
-					message, Toast.LENGTH_SHORT).show(); 
-			
+			if (success != 1) {
+				Toast.makeText(getApplicationContext(), message,
+						Toast.LENGTH_SHORT).show();
+			}
 		}
-		}
+	}
 }
---
+++ Wanna/src/com/example/wanna/SearchGroupResult.java
@@ -0,0 +1,140 @@
+package com.example.wanna;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.example.wanna.library.JSONParser;
+import com.example.wanna.library.ListViewAdapter;
+import com.example.wanna.library.UserFunctions;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class SearchGroupResult extends ListActivity {
+
+	// Creating JSON Parser object
+	JSONParser jsonParser = new JSONParser();
+	UserFunctions userFunctions = new UserFunctions();
+	private ProgressDialog pDialog;
+
+	private String urlSearchGroup = userFunctions.URL_ROOT
+			+ "DB_SearchGroup.php";
+	ListView lvGroupItem;
+
+	ArrayList<String[]> groupItemsList = new ArrayList<String[]>();
+	ListViewAdapter searchGroupAdapter;
+	String groupID;
+	String groupName;
+	int success;
+	String message;
+	String searchType;
+	String searchGroupName = null;
+	String searchGroupCategory = null;
+
+	// group JSONArray
+	JSONArray groupList = null;
+
+	// JSON Node names
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
+	private static final String TAG_GROUPLIST = "groupList";
+	private static final String TAG_GROUPID = "groupID";
+	private static final String TAG_GROUPNAME = "groupName";
+	private static final String TAG_SEARCHGROUPTYPE = "searchType";
+	private static final String TAG_SEARCHGROUPNAME = "searchGroupName";
+	private static final String TAG_SEARCHGROUPCATEGORY = "searchGroupCategory";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_group_result);
+		Intent intent = getIntent();
+		searchType = intent.getStringExtra(TAG_SEARCHGROUPTYPE);
+		if (searchType == "Name") {
+			searchGroupName = intent.getStringExtra(TAG_SEARCHGROUPNAME);
+		} else if (searchType == "Category") {
+			searchGroupCategory = intent
+					.getStringExtra(TAG_SEARCHGROUPCATEGORY);
+		}
+		new SearchGroupTask().execute();
+		lvGroupItem = (ListView) findViewById(android.R.id.list);
+		searchGroupAdapter = new ListViewAdapter(groupItemsList, this);
+		lvGroupItem.setAdapter(searchGroupAdapter);
+	}
+
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		String groupID = (String) searchGroupAdapter.getItem(position);
+		Intent intent = new Intent(getApplicationContext(),
+				ViewAndJoinEvent.class);
+		intent.putExtra("groupID", groupID);
+		startActivity(intent);
+	}
+
+	private class SearchGroupTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(SearchGroupResult.this);
+			pDialog.setTitle("Contacting Servers");
+			pDialog.setMessage("Loading ...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+
+		@Override
+		protected String doInBackground(String... urls) {
+			// Building Parameters
+			List<NameValuePair> searchGroupParams = new ArrayList<NameValuePair>();
+			searchGroupParams.add(new BasicNameValuePair("searchType",
+					searchType));
+			if (searchType == "Name") {
+				searchGroupParams.add(new BasicNameValuePair("searchGroupName",
+						searchGroupName));
+			} else if (searchType == "Category") {
+				searchGroupParams.add(new BasicNameValuePair(
+						"searchGroupCategory", searchGroupCategory));
+			}
+			JSONObject json = jsonParser.getJSONFromUrl(urlSearchGroup,
+					searchGroupParams);
+			// json success tag
+			success = json.optInt(TAG_SUCCESS);
+			message = json.optString(TAG_MESSAGE);
+			if (success == 1) {
+				groupList = json.optJSONArray(TAG_GROUPLIST);
+				// looping through All groups
+				for (int i = 0; i < groupList.length(); i++) {
+					JSONObject event = groupList.optJSONObject(i);
+					groupID = event.optString(TAG_GROUPID);
+					groupName = event.optString(TAG_GROUPNAME);
+					String[] groupItems = { groupID, groupName };
+					groupItemsList.add(groupItems);
+				}
+			} else {
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if (success != 1) {
+				Toast.makeText(getApplicationContext(), message,
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+}
