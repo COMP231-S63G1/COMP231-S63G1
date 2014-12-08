3fe43c31299f898e4418bd8b37e5328bc427c513
YC-COMP231-S63G1 <changyu19882013@gmail.com>
2014-12-08 00:17:18 -0500

    little modification

--- Wanna/src/com/example/wanna/PersonLoginSuccess.java
+++ Wanna/src/com/example/wanna/PersonLoginSuccess.java
@@ -6,14 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 
 import com.example.wanna.library.JSONParser;
 import com.example.wanna.library.LocationTracker;
@@ -61,7 +56,7 @@ public class PersonLoginSuccess extends Activity {
 	UserFunctions userFunctions = new UserFunctions();
 	private String urlCheckLogin = UserFunctions.URL_ROOT + "DB_LoginSuccess.php";
 
-	
+
     /**
      * Called when the activity is first created.
      */
@@ -92,53 +87,61 @@ public class PersonLoginSuccess extends Activity {
         	   Toast.makeText(getApplicationContext(), "Cannot get location",
 						Toast.LENGTH_SHORT).show();
         }
-        new LoginSeccessTask().execute();  
-       
-
-
-}
-    
+        new LoginSeccessTask().execute();        
+    }
     
     public void onCreateEvent(View view){	
 		Intent intent = new Intent(this, CreateEvent.class);
 		startActivity(intent);
 		}
-    public void onCreateGroup(View view){	
-  		Intent intent = new Intent(this, CreateGroup.class);
-  		startActivity(intent);
-  		}  
     
-    public void onSearchUserName(View view){	
-  		Intent intent = new Intent(this, SearchUserName.class);
-  		startActivity(intent);
-  		}  
+    public void onSearchEvent(View view){	
+		Intent intent = new Intent(this, SearchEvent.class);
+		startActivity(intent);
+		}
     
-    public void onCreateProfile(View view){	
-  		Intent intent = new Intent(this, CreatePersonProfile.class);
-  		startActivity(intent);
-  		}  
-  
-    public void myaccountclick(View view){	
-  		Intent intent = new Intent(this, MyAccountPerson.class);
-  		startActivity(intent);
-  		}  
-    public void searcheventclick(View view){	
-  		Intent intent = new Intent(this, SearchEvent.class);
-  		startActivity(intent);
-  		}  
-    public void searchgroupclick(View view){	
-  		Intent intent = new Intent(this, SearchGroup.class);
-  		startActivity(intent);
-  		}  
-    public void searchfilterationclick(View view){	
-  		Intent intent = new Intent(this, SearchUserFiltration.class);
-  		startActivity(intent);
-  		}  
-  
-  
-  
-  
+    public void onViewProfileClick(View view){	
+		Intent intent = new Intent(this, ViewPersonProfile.class);
+		startActivity(intent);
+		}
     
+    public void onChangePasswordClick(View view){	
+		Intent intent = new Intent(this, ChangePassword.class);
+		startActivity(intent);    	
+    }
+    
+    public void onLogoutClick(View view){    	
+		Intent intent = new Intent(this, Login.class);
+		startActivity(intent);    	
+    }
+    
+    public void onSearchGroupClick(View view){	
+		Intent intent = new Intent(this, SearchGroup.class);
+		startActivity(intent);
+		}
+    
+    public void onCreateGroupClick(View view){	
+		Intent intent = new Intent(this, CreateGroup.class);
+		startActivity(intent);
+		}    
+
+    public void onSearchUserClick(View view){	
+		Intent intent = new Intent(this, SearchUserName.class);
+		startActivity(intent);
+		}
+
+    public void onFiltrationUserClick(View view){	
+		Intent intent = new Intent(this, SearchUserFiltration.class);
+		startActivity(intent);
+		}
+    public void onViewFriendClick(View view){
+    	Intent intent = new Intent(this,ViewFriendList.class);
+    	startActivity(intent);
+    }
+    public void onViewNotificationClick(View view){
+    	Intent intent = new Intent(this,ViewNotifications.class);
+    	startActivity(intent);
+    }
     private class LoginSeccessTask extends AsyncTask<String, Void, String> {
 		@Override
 		protected String doInBackground(String... urls) {
